04fcfa9ee71ac0488ff88aa003aa1cb0f82feb71
Hp Victus <tursunovAbd563@gmail.com>
2023-07-26 22:18:46 +0500

    color,svg,image splash uchun yuklandi

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages{
+
+  static const String facebook = "assets/images/facebook.png";
+  static const String google = "assets/images/google.png";
+  static const String ios = "assets/images/ios.png";
+}
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,4 +3,7 @@ import 'dart:ui';
 class AppColors{
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color c_FC6828 = Color(0xFFFC6828);
+  static const Color c_F9FAFB = Color(0xFFF9FAFB);
+  static const Color c_040415 = Color(0xFF040415);
 }
