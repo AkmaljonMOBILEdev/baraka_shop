f8280dd16dbcfc2aadd8969982fc41799b79cc03
Akmaljon <akmaljondeveloper@gmail.com>
2023-07-27 18:20:36 +0500

    splash screen is updated

--- lib/ui/splash_screen/splash_screen.dart
+++ lib/ui/splash_screen/splash_screen.dart
@@ -33,60 +33,65 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.cFC6828,
-      appBar: AppBar(
-        backgroundColor: AppColors.cFC6828,
-        elevation: 0,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 120.h,
-              width: double.infinity,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 140.h),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 25,
-                      bottom: 80,
-                      child: SvgPicture.asset(AppIcons.splashHelperLogo,fit: BoxFit.cover,),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 120.h,
+            width: double.infinity,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 140.h),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 25,
+                    bottom: 80,
+                    child: SvgPicture.asset(AppIcons.splashHelperLogo,fit: BoxFit.cover,),
+                  ),
+                  Positioned(
+                    top: 25.h,
+                    left: 3.w,
+                    child: SvgPicture.asset(
+                      AppIcons.splashMainLogo,
                     ),
-                    Positioned(
-                      top: 25.h,
-                      left: 3.w,
-                      child: SvgPicture.asset(
-                        AppIcons.splashMainLogo,
-                      ),
-                    ),
-                    Positioned(
-                      top: 45.h,
-                      left: 30.w,
-                      child: Text("b",
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayMedium!
-                              .copyWith(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w800,
-                                color: AppColors.cFC6828,
-                              ),),
-                    )
-                  ],
-                ),
+                  ),
+                  Positioned(
+                    top: 45.h,
+                    left: 30.w,
+                    child: Text("b",
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayMedium!
+                            .copyWith(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w800,
+                              color: AppColors.cFC6828,
+                            ),),
+                  )
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "baraka shop",
-                style: Theme.of(context).textTheme.headlineMedium
-              ),
-            ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("baraka shop",
+                style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                    fontFamily: 'Capriola',
+                    fontWeight: FontWeight.w800,
+                    color: AppColors.white,
+                    letterSpacing: 1.1,
+                    shadows: [
+                      Shadow(
+                          color: AppColors.c040415,
+                          blurRadius: 4.0.r,
+                          offset: const Offset(2.2, 2.2)
+                      )
+                    ]
+                )),
+          ),
+        ],
       ),
     );
   }
