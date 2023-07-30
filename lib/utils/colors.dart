c40068d6618b66100be57784a8d56d538fdb277e
Akmaljon <akmaljondeveloper@gmail.com>
2023-07-30 17:50:37 +0500

    little changes done

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,4 +6,7 @@ class AppColors{
   static const Color cFC6828 = Color(0xFFFC6828);
   static const Color cF9FAFB = Color(0xFFF9FAFB);
   static const Color c040415 = Color(0xFF040415);
+  static const Color c246EE9 = Color(0xFFFEDBD0);
+  static const Color c442C2E = Color(0xFF442C2E);
+  static const Color cFEEAE6 = Color(0xFFFEEAE6);
 }
