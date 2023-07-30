93a8d0b22559e5e8aae90b8422083bf7844c4dd6
Akmaljon <akmaljondeveloper@gmail.com>
2023-07-30 08:51:33 +0500

    loosing screen problems are fixed

--- lib/ui/auth_screens/login_screen.dart
+++ lib/ui/auth_screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:baraka_shop/ui/auth_screens/widget/or_auth_widget.dart';
 import 'package:baraka_shop/ui/auth_screens/widget/other_auth_ways.dart';
 import 'package:baraka_shop/ui/widget/global_logo_name.dart';
+import 'package:baraka_shop/utils/app_routes.dart';
 import 'package:baraka_shop/utils/icons.dart';
 import 'package:baraka_shop/utils/sizebox_extension.dart';
 import 'package:flutter/material.dart';
@@ -12,86 +13,88 @@ import '../../utils/colors.dart';
 import '../widget/global_button.dart';
 import '../widget/global_text_field.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key, required this.voidCallback}) : super(key: key);
-final  VoidCallback voidCallback;
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return
-      Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 20.0.w),
-        child: Column(
-          children: [
-            20.ph,
-            Expanded(
-              child: ListView(
-                physics: BouncingScrollPhysics(),
-                children: [
-                  70.ph,
-                    FadeInLeft(child: Center(child:  GlobalLogoName(),)),
-                  70.ph,
-                   FadeInLeft(
-                     child: GlobalTextField(
-                        icon: AppIcons.username,
-                        hintText: "Enter your email",
-                        keyboardType: TextInputType.emailAddress,
-                        textInputAction: TextInputAction.next,
-                        controller: context.read<AuthProvider>().emailController
-                        ),
-                   ),
-                  20.ph,
-                   FadeInRight(
-                     child: GlobalTextField(
-                        icon: AppIcons.password,
-                        hintText: "Enter you password",
-                        keyboardType: TextInputType.visiblePassword,
-                        textInputAction: TextInputAction.done,
-                        controller: context.read<AuthProvider>().passwordController,
-                  ),
-                   ),
-                  70.ph,
-                  FadeInUpBig(
-                    child: GlobalButton(
-                      title: "Login",
-                      onTap: () {
-                        context.read<AuthProvider>().signUpUser(context);
-                      },
+      Scaffold(
+        backgroundColor: AppColors.cFC6828,
+        body: Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 20.0.w),
+          child: Column(
+            children: [
+              20.ph,
+              Expanded(
+                child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    70.ph,
+                      FadeInLeft(child: const Center(child:  GlobalLogoName(),)),
+                    70.ph,
+                     FadeInLeft(
+                       child: GlobalTextField(
+                          icon: AppIcons.username,
+                          hintText: "Enter your email",
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          controller: context.read<AuthProvider>().emailController
+                          ),
+                     ),
+                    20.ph,
+                     FadeInRight(
+                       child: GlobalTextField(
+                          icon: AppIcons.password,
+                          hintText: "Enter you password",
+                          keyboardType: TextInputType.visiblePassword,
+                          textInputAction: TextInputAction.done,
+                          controller: context.read<AuthProvider>().passwordController,
                     ),
-                  ),
-                  50.ph,
-                  const OrAuthWidget(),
-                  60.ph,
-                  const Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 80),
-                    child: OtherAuthWays(),
-                  ),
-                  60.ph,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Don’t have an account?",
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: AppColors.white,
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          voidCallback.call();
-                          context.read<AuthProvider>().signUpPressed();
+                     ),
+                    70.ph,
+                    FadeInUpBig(
+                      child: GlobalButton(
+                        title: "Login",
+                        onTap: () {
+                          context.read<AuthProvider>().login(context);
                         },
-                        child:  Text("Sign Up",
-                          style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                              fontSize: 19.sp, color: AppColors.c040415, fontWeight: FontWeight.w900
-                          )),
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                    50.ph,
+                    const OrAuthWidget(),
+                    60.ph,
+                    const Padding(
+                      padding:  EdgeInsets.symmetric(horizontal: 80),
+                      child: OtherAuthWays(),
+                    ),
+                    60.ph,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don’t have an account?",
+                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: AppColors.white,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            context.read<AuthProvider>().signUpPressed();
+                            Navigator.pushReplacementNamed(context, RouteNames.signUpScreen);
+                          },
+                          child:  Text("Sign Up",
+                            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                                fontSize: 19.sp, color: AppColors.c040415, fontWeight: FontWeight.w900
+                            )),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       );
 
--- lib/ui/auth_screens/sign_up_screen.dart
+++ lib/ui/auth_screens/sign_up_screen.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:baraka_shop/ui/auth_screens/widget/or_auth_widget.dart';
 import 'package:baraka_shop/ui/auth_screens/widget/other_auth_ways.dart';
 import 'package:baraka_shop/ui/widget/global_logo_name.dart';
+import 'package:baraka_shop/utils/app_routes.dart';
 import 'package:baraka_shop/utils/icons.dart';
 import 'package:baraka_shop/utils/sizebox_extension.dart';
 import 'package:flutter/material.dart';
@@ -12,110 +13,112 @@ import '../../utils/colors.dart';
 import '../widget/global_button.dart';
 import '../widget/global_text_field.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({Key? key, required this.voidCallback}) : super(key: key);
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
 
- final VoidCallback voidCallback;
 
   @override
   Widget build(BuildContext context) {
     return
-      Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 20.0.w),
-        child: Column(
-          children: [
-            20.ph,
-            Expanded(
-              child: ListView(
-                physics: const BouncingScrollPhysics(),
-                children: [
-                  20.ph,
-                 FadeInLeft(child:const Center(child: GlobalLogoName(),)),
-                  40.ph,
-                  FadeInRight(
-                    child: GlobalTextField(
-                      icon: AppIcons.username,
-                      hintText: "Enter your username",
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.next,
-                      controller: context.read<AuthProvider>().usernameController,
-
-                    ),
-                  ),
-                  20.ph,
-                  FadeInLeft(
-                    child: GlobalTextField(
-                        icon: AppIcons.email,
-                        hintText: "Enter your email",
-                        keyboardType: TextInputType.emailAddress,
+      Scaffold(
+        backgroundColor: AppColors.cFC6828,
+        body: Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 20.0.w),
+          child: Column(
+            children: [
+              20.ph,
+              Expanded(
+                child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    20.ph,
+                   FadeInLeft(child:const Center(child: GlobalLogoName(),)),
+                    40.ph,
+                    FadeInRight(
+                      child: GlobalTextField(
+                        icon: AppIcons.username,
+                        hintText: "Enter your username",
+                        keyboardType: TextInputType.text,
                         textInputAction: TextInputAction.next,
-                        controller: context.read<AuthProvider>().emailController),
-                  ),
-                  20.ph,
-                  FadeInRight(
-                    child: GlobalTextField(
-                        icon: AppIcons.password,
-                        hintText: "Enter your password",
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.done,
-                        controller: context.read<AuthProvider>().passwordController),
-                  ),
-                  20.ph,
-                  FadeInLeft(
-                    child: GlobalTextField(
-                        icon: AppIcons.password,
-                        hintText: "Confirm  password",
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.done,
-                        controller: context.read<AuthProvider>().passwordController),
-                  ),
-                  50.ph,
-                  FadeInUpBig(
-                    child: GlobalButton(
-                      title: "Sign Up",
-                      onTap: () {
-                        context.read<AuthProvider>().signUpUser(context);
-                      },
-                    ),
-                  ),
-                  40.ph,
-                  const OrAuthWidget(),
-                  50.ph,
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 80),
+                        controller: context.read<AuthProvider>().usernameController,
 
-                    child: OtherAuthWays(),
-                  ),
-                  20.ph,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Do you have an account?",
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              color: AppColors.white,
-                            ),
                       ),
-                      TextButton(
-                        onPressed: () {
-                          context.read<AuthProvider>().loginPressed();
-                         voidCallback.call();
+                    ),
+                    20.ph,
+                    FadeInLeft(
+                      child: GlobalTextField(
+                          icon: AppIcons.email,
+                          hintText: "Enter your email",
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          controller: context.read<AuthProvider>().emailController),
+                    ),
+                    20.ph,
+                    FadeInRight(
+                      child: GlobalTextField(
+                          icon: AppIcons.password,
+                          hintText: "Enter your password",
+                          keyboardType: TextInputType.text,
+                          textInputAction: TextInputAction.done,
+                          controller: context.read<AuthProvider>().passwordController),
+                    ),
+                    20.ph,
+                    FadeInLeft(
+                      child: GlobalTextField(
+                          icon: AppIcons.password,
+                          hintText: "Confirm  password",
+                          keyboardType: TextInputType.text,
+                          textInputAction: TextInputAction.done,
+                          controller: context.read<AuthProvider>().passwordController),
+                    ),
+                    50.ph,
+                    FadeInUpBig(
+                      child: GlobalButton(
+                        title: "Sign Up",
+                        onTap: () {
+                          context.read<AuthProvider>().signUpUser(context);
                         },
-                        style: TextButton.styleFrom(
-                          // backgroundColor: AppColors.c040415,
-                          foregroundColor: AppColors.c040415,
-                        ),
-                        child:  Text("Login",style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                          fontSize: 19.sp, color: AppColors.c040415, fontWeight: FontWeight.w900
-                        ),),
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                    40.ph,
+                    const OrAuthWidget(),
+                    50.ph,
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 80),
+
+                      child: OtherAuthWays(),
+                    ),
+                    20.ph,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Do you have an account?",
+                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                color: AppColors.white,
+                              ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            context.read<AuthProvider>().loginPressed();
+                            Navigator.pushReplacementNamed(context, RouteNames.loginScreen);
+                          },
+                          style: TextButton.styleFrom(
+                            // backgroundColor: AppColors.c040415,
+                            foregroundColor: AppColors.c040415,
+                          ),
+                          child:  Text("Login",style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                            fontSize: 19.sp, color: AppColors.c040415, fontWeight: FontWeight.w900
+                          ),),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       );
 
